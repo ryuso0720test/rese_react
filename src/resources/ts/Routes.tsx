5abecf430bcf51661ef20d4ff56fc809611a34ca
r-takahashi <ryuso0720.work@gmail.com>
2024-07-27 19:44:38 +0900

    不要ファイル削除

--- src/resources/ts/Routes.tsx
+++ src/resources/ts/Routes.tsx
@@ -1,7 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import React, { useEffect, useState, useRef } from "react";
-import Home from "./Home"
-import Form from "./screen/Form"
 import View from "./View"
 import Detail from "./screen/detail"
 import Result from "./screen/result"
@@ -10,16 +7,6 @@ import Thanks from "./screen/thanks"
 import Login from "./screen/login"
 import Register from "./screen/registeration"
 import Item from "./screen/Items"
-import axios from 'axios';
-
-const fetchAuthUser = async () => {
-    axios.get('/api/user').then(response => {
-        console.log('通信成功');
-    })
-    .catch(() => {
-        console.log('通信に失敗しました');
-    });
-}
 
 
 
